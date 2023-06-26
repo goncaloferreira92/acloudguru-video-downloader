c3730e5a9417cd14b1bec73a32ad8df3c4604a65
Gonçalo Ferreira <goncalo.ferreira@datathings.com>
2023-06-26 22:15:43 +0200

    Added manual link parser + iterator for those links to chain download

--- getUrls.js
+++ getUrls.js
@@ -3,14 +3,19 @@
 const cssClassHeader = 'css-pr2tx6';
 
 function getURLs() {
+     const hrefList = [];
      const headers = document.getElementsByClassName(cssClassHeader);
      for (let i = 0; i < headers.length; i++) {
           const header = headers[i];
-          const headerNodeList = Array.from(header.nodeList);
-          // for (let j = 0; j < headerNodeList.length; j++) {
-          //      console.log(headerNodeList[j]);
-          // }
+          const headerNodeList = Array.from(header.childNodes);
+          for (let j = 0; j < headerNodeList.length; j++) {
+               if (headerNodeList[j] instanceof HTMLAnchorElement) {
+                   const href = headerNodeList[j].href;
+                   hrefList.push(href);
+               }
+          }
      }
+    console.log(hrefList); // Copy this to a file
 }
 
 getURLs();
