c8e97e0e5e8219da706ac298ac78ca02046cc0d5
Gonçalo Ferreira <goncalo.ferreira@datathings.com>
2023-06-28 19:13:19 +0200

    Ongoing

--- index.js
+++ index.js
@@ -102,99 +102,48 @@ function openAndDownloadVideo() {
   }
 }
 
-
 const openWindowFunction = `
 
-  function getVideoName() {
-    // The first 2 heading elements give the chapter number and the title
-    const headingElements = document.getElementsByClassName("chakra-heading");
-    const chapter = headingElements[0].innerHTML;
-    const title = headingElements[1].innerHTML;
-    return { chapter, title };
-  }
-
-  async function downloadUrl(url, chapter, title) {
-      await fetch(url)
-        .then((res) => {
-          console.log('1');
-          return res.blob();
-        })
-        .then((file) => {
-          console.log("2");
-          const tempUrl = URL.createObjectURL(file);
-          const aTag = document.createElement("a");
-          aTag.href = tempUrl;
-
-          console.log("3");
-          // // Get video name
-          // if (chapter && title) {
-          //   aTag.download = "test.mp4";
-          // } else {
-          //   const { chapter, title } = getVideoName();
-          //   aTag.download = "test.mp4";
-          // }
-          aTag.download = 'test.mp4';
-
-          console.log('4');
-  
-          document.body.appendChild(aTag);
-          aTag.click();
-          URL.revokeObjectURL(tempUrl);
-          aTag.remove();
-          window.alert("done");
-          res(true);
-        })
-        .catch((err) => {
-          rej(err);
-        });
-  }
-  
-  const LAB_BUTTON_CONTENT = "Start Lab";
-  const QUIZ_BUTTON_CONTENT = "Start quiz";
-  const LAB_ATTRIBUTE_KEY = "data-testid"; // Custom div's attribute that spots the lab's icon
-
-  function openAndDownloadVideo() {
-    // All others are just videos
-    const videoElement = document.getElementsByTagName("video")[0];
-    const url = videoElement.src;
-  
-    if (url) {
-      downloadUrl(url);
-    }
-  }
-
 setTimeout(() => {
-  openAndDownloadVideo();
-}, 15000);
+    const bc = new BroadcastChannel('remote-window');
+    bc.postMessage("close");
+   window.close();
+}, 1000);
 
 `
 
 function createScriptAttribute() {
-  const fileref = document.createElement("script");
+  const fileref = document.createElement('script');
   fileref.setAttribute("type", "text/javascript");
   fileref.innerHTML = openWindowFunction;
   return fileref;
 }
 
 async function openPageAndDownloadVideo(href) {
+  const bc = new BroadcastChannel("remote-window");
   const win = await window.open(href);
   const fileref = createScriptAttribute();
   win.document.head.appendChild(fileref);
-  // // const tempWindow =
-  //   window.location = (href, '_blank');
-  // tempWindow.onload(() => {
-  //   console.log('loaded');
-  // });
+  // const promise = 
+  return new Promise((res, rej) => {
+    bc.onmessage = (e) => {
+      if (e.data === 'closed') {
+        return res();
+      }
+    };
+  });
+  await promise;
 }
 
 async function iterator() {
   // Should iterate through all the list elements here.
   for await (const href of downloadURLs) {
     const anchorEls = document.getElementsByTagName("a");
-    const anchorEl = Array.from(anchorEls).find(
-      (el) => el.href === href && !el.hasAttribute("data-testid")
-    );
-    if (anchorEl && anchorEl instanceof HTMLAnchorElement) {
+    const anchorEl = Array.from(anchorEls).find((el) => el.href === href && !el.hasAttribute("data-testid"));
+    if (
+      anchorEl &&
+      anchorEl instanceof HTMLAnchorElement
+    ) {
       const parentNode = anchorEl.parentNode;
       const chapter = Array.from(
         parentNode.childNodes
