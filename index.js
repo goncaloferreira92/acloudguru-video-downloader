be38383e4048dcc56a07889e67a38a4e6234b3c3
Gonçalo Ferreira <goncalo.ferreira@datathings.com>
2023-06-28 21:41:08 +0200

    Finally working!! :)

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 import downloadURLs from "./downloadUrls";
 
+const openWindowFunction = `
+
 function getVideoName() {
   // The first 2 heading elements give the chapter number and the title
   const headingElements = document.getElementsByClassName("chakra-heading");
@@ -11,139 +13,107 @@ function getVideoName() {
 }
 
 async function downloadUrl(url, chapter, title) {
-  // return new Promise((res, rej) => {
-    await fetch(url)
-      .then((res) => res.blob())
-      .then((file) => {
-        const tempUrl = URL.createObjectURL(file);
-        const aTag = document.createElement("a");
-        aTag.href = tempUrl;
+  await fetch(url)
+    .then((res) => {
+      console.log('downloading...');
+      return res.blob();
+    })
+    .then((file) => {
+      const tempUrl = URL.createObjectURL(file);
+      const aTag = document.createElement("a");
+      aTag.href = tempUrl;
 
-        // Get video name
-        if (chapter && title) {
-          aTag.download = `${chapter} - ${title}.mp4`;
-        } else {
-          const { chapter, title } = getVideoName();
-          aTag.download = `${chapter} - ${title}.mp4`;
-        }
+      // Get video name
+      if (chapter && title) {
+        aTag.download = chapter + " - " + title + ".mp4";
+      } else {
+        const { chapter, title } = getVideoName();
+        aTag.download = chapter + " - " + title + ".mp4";
+      }
 
-        document.body.appendChild(aTag);
-        aTag.click();
-        URL.revokeObjectURL(tempUrl);
-        aTag.remove();
-        res(true);
-      })
-      .catch((err) => {
-        rej(err);
-      });
-  // });
+      document.body.appendChild(aTag);
+      aTag.click();
+      URL.revokeObjectURL(tempUrl);
+      aTag.remove();
+      res(true);
+    })
+    .catch((err) => {
+      rej(err);
+    });
 }
 
-// /**
-//  *
-//  * @param {HTMLDivElement} labButton
-//  */
-// function labProcess(labButton) {
-//   labButton.firstChild.click();
-//   window.addEventListener("hashchange", () => {
-//     window.addEventListener("onload", () => {
-//       console.log("window moved and changed");
-//     });
-//   });
-// }
+function closeWindow() {
+  const bc = new BroadcastChannel('remote-window');
+  bc.postMessage("close");
+  window.close();
+}
 
-// /**
-//  *
-//  * @param {HTMLDivElement} quizButton
-//  */
-// function quizProcess(quizButton) {
-//   // Do whatever
-// }
-
-// const LAB_BUTTON_CONTENT = "Start Lab";
-// const QUIZ_BUTTON_CONTENT = "Start quiz";
-// const LAB_ATTRIBUTE_KEY = "data-testid"; // Custom div's attribute that spots the lab's icon
-
-function openAndDownloadVideo() {
-  // Detect if it is lab or quiz
-  let labButton;
-  let quizButton;
-  const divElements = document.getElementsByTagName("div");
-  for (let i = 0; i < divElements.length; i++) {
-    const el = divElements[i];
-    if (el.hasAttribute(LAB_ATTRIBUTE_KEY)) {
-      // Lab case
-      if (el.innerText === LAB_BUTTON_CONTENT) {
-        labButton = el;
-        break;
-      }
-      // Quiz case
-      if ((el.innerText = QUIZ_BUTTON_CONTENT)) {
-        quizButton = el;
-        break;
-      }
-    }
-  }
-
-  if (labButton) {
-    labProcess(labButton);
-    return;
-  } else if (quizButton) {
-    quizProcess(quizButton);
-    return;
-  }
-
+async function openAndDownloadVideo() {
   // All others are just videos
   const videoElement = document.getElementsByTagName("video")[0];
-  const url = videoElement.src;
-
-  if (url) {
-    downloadUrl(url);
+  
+  let url;
+  if (videoElement) {
+    url = videoElement.src;
+  }
+  
+  if (url != null) {
+    // triggerTimeout.clearTimeout(triggerTimeout);
+    console.log('found url: ', url);
+    await downloadUrl(url);
+    closeWindow();
+  } else {
+    triggerTimeout = setTimeout(() => {
+      console.log('retrying...');
+      openAndDownloadVideo();
+    }, 1000);
+    return;
   }
 }
 
-const openWindowFunction = `
-
-setTimeout(() => {
-    const bc = new BroadcastChannel('remote-window');
-    bc.postMessage("close");
-   window.close();
+let triggerTimeout = setTimeout(() => {
+    console.log('timeout triggered');
+    openAndDownloadVideo();
 }, 1000);
 
-`
+triggerTimeout();
+
+`;
 
 function createScriptAttribute() {
-  const fileref = document.createElement('script');
+  const fileref = document.createElement("script");
   fileref.setAttribute("type", "text/javascript");
   fileref.innerHTML = openWindowFunction;
   return fileref;
 }
 
+// function defineWindowFunction(window) {
+//   if (window instanceof Window) {
+//     window.testFunc = () => alert("test function");
+//   }
+// }
+
 async function openPageAndDownloadVideo(href) {
   const bc = new BroadcastChannel("remote-window");
-  const win = await window.open(href);
+  const win = window.open(href);
   const fileref = createScriptAttribute();
   win.document.head.appendChild(fileref);
-  // const promise = 
-  return new Promise((res, rej) => {
-    bc.onmessage = (e) => {
-      if (e.data === 'closed') {
-        return res();
-      }
+  return new Promise((res) => {
+    bc.onmessage = () => {
+      return res();
     };
   });
-  await promise;
+  // await promise;
 }
 
 async function iterator() {
   // Should iterate through all the list elements here.
   for await (const href of downloadURLs) {
     const anchorEls = document.getElementsByTagName("a");
-    const anchorEl = Array.from(anchorEls).find((el) => el.href === href && !el.hasAttribute("data-testid"));
-    if (
-      anchorEl &&
-      anchorEl instanceof HTMLAnchorElement
-    ) {
+    const anchorEl = Array.from(anchorEls).find(
+      (el) => el.href === href && !el.hasAttribute("data-testid")
+    );
+    if (anchorEl && anchorEl instanceof HTMLAnchorElement) {
       const parentNode = anchorEl.parentNode;
       const chapter = Array.from(
         parentNode.childNodes
